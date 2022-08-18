97ab2deedaa232c2bcaf5bb59b38ba3c2529d550
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-08-18 16:35:40 +0200

    done

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -1,10 +1,30 @@
+//Global variables
 const currency = 'USD';
-const start = '2013-09-01';
-const end = '2014-09-01';
-
+let start = '2013-09-01'; //Initial value for start-date
+let end = '2014-09-01'; //Initial value for end-date 
 const apiBase = 'http://api.coindesk.com/v1/bpi/historical/close.json';
 
-axios
+//Add graph on load
+update();
+
+//Query Selectors
+let startInput = document.querySelector('#start-date');
+let endInput = document.querySelector('#end-date');
+
+//Event listeners
+startInput.addEventListener('change', (e) => {
+    start = startInput.value; 
+    update();
+});
+
+endInput.addEventListener('change', (e) => {
+    end = endInput.value;
+    update();
+})
+
+//Update function 
+function update(){
+    axios
     .get(`${apiBase}?start=${start}&end=${end}&currency=${currency}`)
     .then((response) => {
 
@@ -16,7 +36,9 @@ axios
         printChart(dates, values);
     })
     .catch(err => console.log(err));
+}
 
+//Print chart function
 function printChart(dates, values) {
     //Setting context for canvas
     const ctx = document.getElementById('chart').getContext('2d');
@@ -27,8 +49,8 @@ function printChart(dates, values) {
             labels: dates,
             datasets: [{
                 label: "BTC Prices",
-                backgroundColor: '#000000',
-                borderColor: '#FFFFFF',
+                backgroundColor: '#FFFFFF',
+                borderColor: '#0f6cff',
                 data: values
             }],
         },
